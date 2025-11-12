ba70d7683d8c48166b56f432206ae86cf48e55b4
consoleillusion <consoleillusion@proton.me>
2025-11-13 02:14:17 +1300

    update

--- src/loadPropDefaults.js
+++ src/loadPropDefaults.js
@@ -1,10 +1,24 @@
 import {mergeDeepRight} from 'ramda'
 import $RefParser from "@apidevtools/json-schema-ref-parser";
+import Ajv from 'ajv'
+const ajv = new Ajv()
+
+const schema = {
+  type: "object",
+  properties: {
+    foo: {type: "integer"},
+    bar: {type: "string"}
+  },
+  required: ["foo"],
+  additionalProperties: false
+}
+
 
 export const loadPropDefaults = 
   async (configPath,passedProps) => {
     const openui = await $RefParser.dereference(configPath)
-    const propDefaults = Object.entries(openui.props).reduce( (acc,prop) => {
+    const validate = ajv.compile(openui)
+    const propDefaults = Object.entries(openui.properties).reduce( (acc,prop) => {
       const propName = prop[0]
       const propConfig = prop[1]
       acc[propName] = ((typeof propConfig.default) != undefined) ? propConfig.default : ""
