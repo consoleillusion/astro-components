ab0da025a160491dbb2a7aaa9c208e17d438d396
consoleillusion <consoleillusion@proton.me>
2025-11-14 01:28:40 +1300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import yaml from '@rollup/plugin-yaml'
 //import adapter from '@astrojs/adapter-static'
   
 const fontsourceFonts = ["Mulish", "Fraunces", "Manrope", "Playfair Display", "Source Sans Pro", "Alice", "Quattrocento", "Quattrocento Sans", "Fanwood Text", "Oswald", "Fjalla One", "Libre Baskerville", "Lustria", "Lato", "Cormorant", "Cormorant Garamond", "Proza Libre", "EB Garamond", "Cinzel", "Fauna One", "Sacramento", "Yeseva One", "Josefin Sans", "Montserrat", "Cardo", "Lora", "Roboto", "Spectral", "Karla", "Halant", "Nunito Sans", "Merriweather", "Nunito", "Quicksand", "Ubuntu", "Open Sans", "Hind", "PT Sans", "Crimson Text", "Open Sans", "Arvo", "Abril Fatface", "Poppins", "Playfair Display", "Inconsolata", "Ultra", "Slabo 27px", "Nixie One", "Ledger", "Stint Ultra Expanded", "Pontano Sans", "Amatic SC", "Andika", "Unica One", "Philosopher", "Source Serif Pro", "Cantarell", "Work Sans", "Oxygen", "PT Sans", "Cabin", "Roboto Condensed", "Raleway", "Inter"]
-const adobeFonts = ["Muli Regular"]
+const adobeFonts = []
 
 
 // https://astro.build/config
