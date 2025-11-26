ddc5352a52e10f01d96bd5d521d6e2ba910f2854
consoleillusion <consoleillusion@proton.me>
2025-11-26 18:47:41 +1300

    update

---
+++ index.js
@@ -0,0 +1,31 @@
+import path from 'path'
+import $RefParser from '@apidevtools/json-schema-ref-parser'
+
+export { default as Accordion } from './src/components/Accordion/index.astro';
+export { default as Button } from './src/components/Button/index.astro';
+export { default as Card } from './src/components/Card/index.astro';
+export { default as CardGrid } from './src/components/CardGrid/index.astro';
+export { default as Column } from './src/components/Column/index.astro';
+export { default as Field } from './src/components/Field/index.astro';
+export { default as Figure } from './src/components/Figure/index.astro';
+export { default as Footer } from './src/components/Footer/index.astro';
+export { default as Hero } from './src/components/Hero/index.astro';
+export { default as Link } from './src/components/Link/index.astro';
+export { default as Markdown } from './src/components/Markdown/index.astro';
+export { default as Map } from './src/components/Map/index.astro';
+export { default as Navbar } from './src/components/Navbar/index.astro';
+export { default as OpeningHours } from './src/components/OpeningHours/index.astro';
+export { default as PullQuote } from './src/components/PullQuote/index.astro';
+export { default as Quote } from './src/components/Quote/index.astro';
+export { default as Review } from './src/components/Review/index.astro';
+export { default as SEO } from './src/components/SEO/index.astro';
+export { default as Theme } from './src/components/Theme/index.astro';
+
+const loadSchema = async p => await $RefParser.dereference(path.resolve(import.meta.dir, 'src/components',p, 'config.yaml'))
+/*
+console.log(await configPath('Quote'))
+const loadSchema = 
+const schema = await $RefParser.dereference(configPath)
+console.log(schema)
+*/
+export const schema = await ["Accordion","Button","Card","CardGrid","Column","Field","Figure","Footer","Hero","Link","Markdown","Map","Navbar","OpeningHours","PullQuote","Quote","Review","SEO","Theme"].reduce(async (acc,name)=>Object.assign(await acc,{[name]: await loadSchema(name)}),{})
---
+++ test.js
@@ -0,0 +1,3 @@
+import {schema} from './index.js'
+
+console.log(schema.B)
