e10d90fb3f95c001079182a1ec8366fbcb1a0698
consoleillusion <consoleillusion@proton.me>
2025-11-29 03:58:24 +1300

    update

--- schema.js
+++ schema.js
@@ -1,7 +1,7 @@
 import path from 'path'
 import $RefParser from '@apidevtools/json-schema-ref-parser'
 import {schema as WebPageSchema} from  './src/components/WebPage/config.js'
-const loadSchema = async p => await $RefParser.dereference(path.resolve(import.meta.dir, 'src/components',p, 'config.yaml'))
+const loadSchema = async p => await $RefParser.dereference(path.resolve(import.meta.dirname, 'src/components',p, 'config.yaml'))
 const componentList = ["Accordion","Button","Card","CardGrid","Column","Field","Figure","Footer","Hero","Link","Markdown","Map","Navbar","OpeningHours","PullQuote","Quote","Review","SEO","Theme"]
 export const schema =
   { ...(await componentList.reduce(async (acc,name)=>Object.assign(await acc,{[name]: await loadSchema(name)}),{}))
